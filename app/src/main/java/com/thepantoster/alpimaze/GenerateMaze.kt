dbe72c062ac607ff13911bded9993d715951ebb7
piotr102k <piotrswiszcz2002@wp.pl>
2024-04-17 14:32:44 +0200

    single-block

---
+++ app/src/main/java/com/thepantoster/alpimaze/GenerateMaze.kt
@@ -0,0 +1,4 @@
+package com.thepantoster.alpimaze
+
+class GenerateMaze {
+}
---
+++ app/src/main/java/com/thepantoster/alpimaze/SingleBlock.kt
@@ -0,0 +1,13 @@
+package com.thepantoster.alpimaze
+
+import android.graphics.Color
+
+
+class SingleBlock (c:Int,x:Double,y:Double,s:Double,t:BlockType){
+    val color:Int=c
+    val positionX:Double=x
+    val positionY:Double=y
+    val size:Double=s
+    val type:BlockType=t
+
+}
