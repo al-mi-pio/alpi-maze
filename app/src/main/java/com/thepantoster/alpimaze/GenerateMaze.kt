3d44974da2cfd5488c090feee3da93a00c84dc48
piotr102k <piotrswiszcz2002@wp.pl>
2024-04-17 14:42:38 +0200

    empty-maze

--- app/src/main/java/com/thepantoster/alpimaze/GenerateMaze.kt
+++ app/src/main/java/com/thepantoster/alpimaze/GenerateMaze.kt
@@ -1,4 +1,13 @@
 package com.thepantoster.alpimaze
 
-class GenerateMaze {
+
+import kotlin.random.Random
+
+class GenerateMaze(h:Int,w:Int) {
+    var height:Int=h
+    var width:Int=w
+    var startPosition = arrayOf(0,0)
+    var endPosition = arrayOf(0,0)
+    var mazeLayout = Array(h) { Array<BlockType>(w) { BlockType.floor } }
+
 }
